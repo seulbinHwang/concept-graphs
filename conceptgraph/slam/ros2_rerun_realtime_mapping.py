d11cbd2eb4f1dfc07ecfb2576038b31ea4a4e838
h.sb <jksg01019@gmail.com>
2024-09-07 23:11:02 +0900

    doing

--- conceptgraph/slam/ros2_rerun_realtime_mapping.py
+++ conceptgraph/slam/ros2_rerun_realtime_mapping.py
@@ -217,7 +217,7 @@ class RealtimeHumanSegmenterNode(Node):
         - 부탁드려서, rgb 보내는 것과 depth 보내는 것의 timestamp를 동일하게 해달라고 부탁 
         """
         self.ts = message_filters.ApproximateTimeSynchronizer(
-            [rgb_sub, depth_sub], queue_size=10, slop=0.05)
+            [rgb_sub, depth_sub], queue_size=10, slop=0.02)
         self.ts.registerCallback(self.sync_callback)
 
     @staticmethod
