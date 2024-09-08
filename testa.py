b1e7cdec9550b5849c536344e7a3aaaf2c01dd2b
h.sb <jksg01019@gmail.com>
2024-09-08 17:06:25 +0900

    doing

--- testa.py
+++ testa.py
@@ -1,4 +1,4 @@
 import open3d as o3d
 aabb = o3d.geometry.AxisAlignedBoundingBox([0, 0, 0], [1, 1, 1])
 print(aabb.get_center())  # center 확인
-print(aabb.extent)  # extent 확인
+print(aabb.get_extent())  # extent 확인
